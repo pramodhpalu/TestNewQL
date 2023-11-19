3e1a8596aec0e447c9f1f7c470355af4e4f98aac
Sahiti <47293116+pramodhpalu@users.noreply.github.com>
2023-11-19 05:58:37 +0530

    Update ViewController.swift

--- Code Sample/Code Sample/ViewController.swift
+++ Code Sample/Code Sample/ViewController.swift
@@ -12,7 +12,35 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // Example usage
+        let url1 = "https://example.com"
+        let url2 = "malicious_script.com"
+
+        if validateURLWithoutSanitization(urlString: url1) {
+            print("URL 1 is valid")
+        } else {
+            print("URL 1 is invalid")
+        }
+
+        if validateURLWithoutSanitization(urlString: url2) {
+            print("URL 2 is valid")
+        } else {
+            print("URL 2 is invalid")
+        }
     }
+    
+
+    func validateURLWithoutSanitization(urlString: String) -> Bool {
+        let regex = try! NSRegularExpression(pattern: "^(https?://)?([\\da-zA-Z.-]+)\\.([a-zA-Z.]{2,6})([/\\w.-]*)*/?$", options: .caseInsensitive)
+        
+        // Match the URL string against the regular expression
+        let range = NSRange(location: 0, length: urlString.utf16.count)
+        return regex.firstMatch(in: urlString, options: [], range: range) != nil
+    }
+
+   
+
 
 
 }
